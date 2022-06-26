ff8162f4eca07054955b9334748afbfd3769d501
oramo1289 <oscar.ramo1289@gmail.com>
2022-06-26 18:06:38 -0500

    solved interview

---
+++ src/js/main.js
@@ -0,0 +1,45 @@
+let reportsWidget = {
+    options: {
+        containerSelector: '.reports',
+        template: (
+            '{{#.}}' +
+                '<article class="reports_item">' +
+                    '<a href="{{cover}}" target="_blank">' +
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'+
+                    '</a>' +
+                    '<footer class="reports_docs">' +
+                        '<div>' +
+                            '{{#documents}}' +
+                                '<h3 class="reports_title">' +
+                                    '<a href="{{url}}" target="_blank">{{title}}</a>' +
+                                '</h3>' +
+                            '{{/documents}}' +
+                        '</div>' +
+
+                        '<div>' +
+                            '{{#documents}}' +
+                                '<h3 class="reports_title">' +
+                                    '<a class="reports_info" href="{{url}}" target="_blank">{{file_size}}</a>' +
+                                '</h3>' +
+                            '{{/documents}}' +
+                        '</div>' +
+
+                    '</footer>' +
+                '</article>' +
+            '{{/.}}'
+        )
+    },
+
+    init: function() {
+        this.renderReports(reportData || []);
+    },
+
+    renderReports: function(reports) {
+        let inst = this,
+            options = inst.options;
+
+        $(options.containerSelector).html(Mustache.render(options.template, reports));
+    }
+};
+
+reportsWidget.init();
